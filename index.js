bc979ce794e2cf37d8713e56d6448566ac13c1c5
Kiri Kiri <killian.podhajsky@gmail.com>
2023-05-31 17:49:49 -0700

    Updating code previously not pushed

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateHTML = ('./util/generateHTML');
-//const Employee = require('./Develop/Lib/Employee');
-//const Manager = require('./Develop/Lib/Manager.js');
-//const Intern = require('./Develop/Lib/Intern');
-//const Engineer = require ('./Develop/Lib/engineer');
-const createPage = require('./Develop/util/generateHTML');
+const generateTeam = ('./util/generateHTML.js');
+const path = require("path");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "index.html");
+const Manager = require('./Develop/Lib/Manager');
 const Intern = require('./Develop/Lib/Intern');
+const Engineer = require ('./Develop/Lib/engineer');
+
+
 
 const team = [];
 
@@ -36,64 +38,36 @@ function menu() {
 
     function managerQuestions() {
         inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Please enter the manager name',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the name');
-                    return false;
-                }
-            }
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter the manager name',
+            
 
     },
     {
         type: 'input',
-       name: 'id',
-       message: 'Please enter manager id',
-       validate: idInput => {
-           if(idInput) {
-               return true;
-           } else {
-               console.log('Please enter correct id');
-                   return false;
-               }
-               }
-           },
+        name: 'id',
+        message: 'Please enter manager id',
+       
+        },
 
-    {
+        {
         type: 'input',
         name: 'email',
         message: 'Please enter email',
-        validate: emailInput => {
-            if(emailInput) {
-                return true;
-            } else {
-                console.log('Please enter correct email');
-                return false;
-            }
-            }
+      
         },
         {
             type: 'number',
             name: 'officeNumber',
             message: 'Please enter Office Phone Number',
-            validate: officeNumberInput => {
-                if(officeNumberInput) {
-                    return true;
-                } else {
-                    console.log('Please enter correct phone number');
-                    return false;
-            }
-            }
         },
+
     ]).then( (templateData) => {
-                const manager = manager(templateData.name, templateData.email, templateData.officeNumber);
-                this.teamArray.push(manager);
-                menu();
+                const manager = new Manager(templateData.name, templateData.email, templateData.officeNumber);
+                this.team.push(manager);
+                addNew();
             });
             
          }
@@ -203,15 +177,15 @@ function menu() {
                 },
                    
         ]).then( (templateData) => {
-             const newIntern = newIntern(templateData.name, templateData.id, templateData.school);
+             const newIntern = Intern(templateData.name, templateData.id, templateData.school);
                 this.teamArray.push(newIntern);
             menu();
         });
     }
         
-    function generateTeam() {
+    function generateFile() {
         console.log('Team Generation Complete')
-        fs.writeFile('./main/index.html')
+        fs.writeFile('./util/generateHTML')
         renderTeam(teamArray), function (err) {
             if (err) {
                 return console.log(err)
